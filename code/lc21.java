e26e1b72b57244e02c8121ec0e1e8009d2368dc9
luocai <31661186+luocai@users.noreply.github.com>
2019-03-25 19:26:50 +0800

    Update lc21.java

--- code/lc21.java
+++ code/lc21.java
@@ -7,7 +7,7 @@ package code;
  * Tips：可用递归的写法，代码更简洁
  */
 public class lc21 {
-
+    //非递归写法
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode res = new ListNode(0);
         ListNode temp = res;
@@ -29,7 +29,25 @@ public class lc21 {
         return res.next;
     }
 
-
+     // 递归写法
+     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+       
+        if(l1 == null)
+            return l2;
+        if(l2 == null)
+            return l1;
+        
+        ListNode res = null;
+        if(l1.val < l2.val){
+            res = l1;
+            l1.next = mergeTwoLists(l1.next, l2);
+        }else{
+            res = l2;
+            l2.next = mergeTwoLists(l1, l2.next);
+        }
+       
+        return res;
+    }
     public class ListNode {
         int val;
         ListNode next;
