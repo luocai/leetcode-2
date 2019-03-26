d3db8bff2b6bbeae2974a8b7bc8e65d7acdf18ad
luocai <31661186+luocai@users.noreply.github.com>
2019-03-26 23:13:45 +0800

    Update lc34.java

--- code/lc34.java
+++ code/lc34.java
@@ -15,35 +15,78 @@ public class lc34 {
         System.out.println(res[1]);
     }
 
-    public static int[] searchRange(int[] nums, int target) {
-        int[] res = {-1,-1};
-        if(nums.length==0)
-            return res;
-        int start = 0;
-        int end = nums.length-1;
-        while(start<end){
-            int mid = (start+end)/2;
-            if(nums[mid]<target){
-                start = mid + 1;
-            }else{  //当==时，起始位置在左边
-                end = mid;
-            }
-        }
-        if(nums[start]!=target)
-            return res;
-        res[0] = start;
-        end = nums.length-1;    //不需要设置start=0了
-        while(start<end){
-            int mid = (start+end)/2+1;  //+1使其偏向右边，否则会死循环
-            if(nums[mid]>target){
-                end = mid-1;
-            }else{
-                start = mid;
-            }
-        }
-        if(nums[start]!=target)
-            return res;
-        res[1]=end;
+     //思路，用二分 分别 找第一和最后出现的
+    public int[] searchRange(int[] nums, int target) {
+        
+        int[] res = new int[2];
+        
+        res[0] = serachF(nums, target);
+        res[1] = serachE(nums,target);
+        
         return res;
     }
+    
+    // 先用二分找第一个出现的，注意 判断条件（一个等号的差别）
+    public int serachF(int[] nums,int target){
+        
+        int i = 0, j  = nums.length-1;
+        
+        while(i <= j){
+            
+            int m = (i +j ) / 2;
+            
+            if(nums[m] >= target){
+                j = m-1;
+            }else {
+                i = m+1;
+            }
+        }
+        if(i < nums.length && nums[i] == target)
+            return i;
+        else 
+            return -1;
+        
+    }
+    
+     // 再用二分找最后一个出现的，注意 判断条件（一个等号的差别）
+    public int serachE(int[] nums, int target){
+        
+        int i = 0, j  = nums.length-1;
+        
+        while(i <= j){
+            
+            int m = (i +j ) / 2;
+            
+            if(nums[m] > target){
+                j = m-1;
+            }else {
+                i = m+1;
+            }
+        }
+        if(j >= 0 && nums[j] == target)
+            return j;
+        else
+            return -1;
+
+    }
+    
+    
+    //法二：先用二分找出位置，在向两边延伸
+     public int[] searchRange(int[] nums, int target){
+		
+		//int flag = 0;
+		for(int i = 0; i < nums.length; i++){
+			if(nums[i] == target){
+			//	flag = 1;
+				int j = i;
+				while(j <= nums.length-2 && nums[j+1] == target){
+					j++;
+				}
+				return new int[]{i, j};
+			}
+		}
+		return new int[] {-1,-1};
+		
+	}
+    
 }
