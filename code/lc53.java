c40a37ca4ac4c0334150cac07846485bad63ee63
luocai <31661186+luocai@users.noreply.github.com>
2019-03-27 14:00:29 +0800

    Update lc53.java

--- code/lc53.java
+++ code/lc53.java
@@ -13,42 +13,51 @@ public class lc53 {
         System.out.println(maxSubArray2(nums));
     }
 
-    public static int maxSubArray(int[] nums) {
-        // dp[i] 表示以nums[i]结尾的最大和
+   // dp  dp[i] 表示以i结尾的最大子序和
+    public int maxSubArray(int[] nums) {
+        
+        if(nums.length == 0)
+            return 0;
         int[] dp = new int[nums.length];
+        
         dp[0] = nums[0];
+        
         int res = dp[0];
-        for (int i = 1; i <nums.length ; i++) {
-            dp[i] = dp[i-1]>0 ? dp[i-1]+nums[i] : nums[i];
-            res = Math.max(res,dp[i]);
+        for(int i = 1; i< nums.length;i++){
+            
+            //如果以i-1结尾的最大子序和小于0了，那我就不加他了，自立门户..
+            if(dp[i-1] >= 0){
+                dp[i] = dp[i-1] + nums[i];
+            }else{
+                dp[i] = nums[i];
+            }
+            if(res < dp[i])
+                res = dp[i];
+        }
+        
+        return res;
+        
+    }
+    
+    
+      // 也是dp ， 做了优化 空间复杂度优化为 o(1)
+    public int maxSubArray(int[] nums) {
+        
+        if(nums.length == 0)
+            return 0;
+        
+        int ans = 0;
+        int res = nums[0];
+        for(int i = 0;i < nums.length;i++){
+            if(ans < 0){
+                ans = 0;
+            }
+            ans += nums[i];
+            if(ans > res)
+                res = ans;
         }
         return res;
     }
-
-    public static int maxSubArray2(int[] nums) {
-        return DivideConquer(nums,0,nums.length-1);
-    }
-
-    public static int DivideConquer(int[] nums, int start, int end){
-        if(start == end)
-            return nums[start];
-        else{
-            int mid = (start+end)/2;
-            int left = DivideConquer(nums, start, mid);
-            int right = DivideConquer(nums, mid+1, end);
-            int temp = 0;
-            int lmax = nums[mid];
-            for (int i = mid; i >=start ; i--) {
-                temp += nums[i];
-                lmax = Math.max(temp,lmax);
-            }
-            temp = 0;
-            int rmax = nums[mid+1];
-            for (int i = mid+1; i <= end ; i++) {
-                temp += nums[i];
-                rmax = Math.max(temp,rmax);
-            }
-            return Math.max(Math.max(left,right),lmax+rmax);
-        }
-    }
+    
+    
 }
