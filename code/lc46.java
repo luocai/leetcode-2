1889c7256845c0980454da9a06d5989a37331724
luocai <31661186+luocai@users.noreply.github.com>
2019-03-27 08:53:07 +0800

    Update lc46.java

--- code/lc46.java
+++ code/lc46.java
@@ -11,29 +11,36 @@ import java.util.List;
  * 思路：典型的回溯题,注意判断下相同元素重复添加,和lc39,lc78做比较
  */
 public class lc46 {
-    public static void main(String[] args) {
-        int nums[] = {};
-        System.out.println(permute(nums).toString());
-    }
-
-    public static List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        backtracking(res,nums,new ArrayList());
+    
+    //典型的回溯问题
+    List<List<Integer>> res = new ArrayList();
+    List<Integer> tres = new ArrayList();
+    public List<List<Integer>> permute(int[] nums) {
+        
+        solution(nums);
+        
         return res;
     }
-    public static void backtracking(List<List<Integer>> res, int[] nums, List l){
-        if(l.size()==nums.length){
-            res.add(new ArrayList<>(l));
+    
+    public void solution(int[] nums){
+        
+        if(tres.size() == nums.length){
+           
+            res.add(new ArrayList(tres));
+           
             return;
         }
-
-        for (int i = 0; i < nums.length ; i++) {
-            if(l.contains((Integer)nums[i]))    //防止相同的元素再次添加
+        
+        for(int i = 0; i < nums.length;i++){
+            
+            if(tres.contains((Integer)nums[i])) //判断是否已经包含
                 continue;
-            l.add(nums[i]);
-            backtracking(res,nums,l);
-            l.remove((Integer)nums[i]);
+            
+            tres.add(nums[i]);
+            solution(nums);
+            tres.remove((Integer)nums[i]);
+            
         }
+        
     }
-
 }
