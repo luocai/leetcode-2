34d5b54f1400feb696bda7911fbbca631619d93e
luocai <31661186+luocai@users.noreply.github.com>
2019-03-28 07:42:49 +0800

    Update lc62.java

--- code/lc62.java
+++ code/lc62.java
@@ -11,20 +11,25 @@ public class lc62 {
         System.out.println(uniquePaths(7,3));
     }
 
-    public static int uniquePaths(int m, int n) {
-        int[][] arr = new int[m][n];
-        for (int i = 0; i < m ; i++) {
-            for (int j = 0; j < n ; j++) {
-                if(i==0 && j==0)
-                    arr[i][j] = 1;
-                else if(i==0)
-                    arr[i][j] = arr[i][j-1];
-                else if(j==0)
-                    arr[i][j] = arr[i-1][j];
-                else
-                    arr[i][j] = arr[i-1][j]+arr[i][j-1];
-            }
-        }
-        return arr[m-1][n-1];
+    // dp     dp[i][j] = dp[i-1][j] + dp[i][j-1]
+   public int uniquePaths(int m, int n) {
+
+		int[][] dp = new int[m][n];
+		
+		for(int i = 0;  i < n; i++){
+			dp[0][i] = 1;
+		}
+		for(int i = 0; i < m; i++){
+			dp[i][0] = 1;
+		}
+		
+		for(int i = 1; i < m; i++){
+			for(int j = 1; j < n; j++){
+				
+				dp[i][j] = dp[i-1][j] + dp[i][j-1];
+			}
+		}
+		
+		return dp[m-1][n-1];
     }
 }
