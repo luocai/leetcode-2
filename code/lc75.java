9f2eedf0105eb42ea27d0416fc49abef19743c2c
luocai <31661186+luocai@users.noreply.github.com>
2019-03-28 13:14:22 +0800

    Update lc75.java

--- code/lc75.java
+++ code/lc75.java
@@ -8,30 +8,30 @@ package code;
  * Tips：一个for循环就可以了，注意避免自己与自己交换，造成l>i
  */
 public class lc75 {
-    public static void main(String[] args) {
-        int[] nums = {1,0,2};
-        sortColors(nums);
-        for (int i = 0; i < nums.length ; i++) {
-            System.out.println(nums[i]);
+    
+    // O(N), 还有一种解法是统计 一轮遍历统计 0 1 2的个数
+      public void sortColors(int[] nums) {
+        
+        int l = 0, r = nums.length-1, i =0;
+        
+        while(i <= r){
+            
+            if(nums[i] == 0){
+                // 前面的i都是 ok了的，所以可以++
+                swap(nums,l++, i++);
+            }else if(nums[i] == 2){
+                //后面的i可能有0，1，所以换了之后还不能++
+                swap(nums,r--, i);
+            }else{
+                i++;
+            }
+            
         }
     }
-    public static void sortColors(int[] nums) {
-        int l = 0;
-        int r = nums.length-1;
-        for (int i = 0; i <= r ; i++) {  // i<r而不是length, 否则又换回来了
-            if(nums[i]==0 && i!=l ){    //避免自己与自己交换
-                int temp = nums[l];
-                nums[l] = nums[i];
-                nums[i] = temp;
-                l++;
-                i--;
-            }else if(nums[i]==2){
-                int temp = nums[r];
-                nums[r] = nums[i];
-                nums[i] = temp;
-                r--;
-                i--;
-            }
-        }
+    
+    public void swap(int[] nums, int l, int r){
+        int temp = nums[l];
+        nums[l] = nums[r];
+        nums[r] = temp;
     }
 }
