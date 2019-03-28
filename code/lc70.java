1b89aef751df0bf2d5ceb9513af975887b362e3a
luocai <31661186+luocai@users.noreply.github.com>
2019-03-28 08:29:37 +0800

    Update lc70.java

--- code/lc70.java
+++ code/lc70.java
@@ -10,17 +10,39 @@ public class lc70 {
         System.out.println(climbStairs(3));
     }
 
-    public static int climbStairs(int n) {
-        if(n==1)
-            return 1;
-        if(n==2)
-            return 2;
+   // dp
+  public int climbStairs(int n) {
+        
         int[] dp = new int[n];
+        
+        if(n <= 2)
+            return n;
+        
         dp[0] = 1;
         dp[1] = 2;
-        for (int i = 2; i < n ; i++) {
+        
+        for(int i = 2 ;i < n; i++){
             dp[i] = dp[i-1] + dp[i-2];
         }
+        
         return dp[n-1];
+        
+        
+    }
+    
+    // o(1)空间
+    public int climbStairs(int n) {
+        
+        if(n == 1 || n== 2)
+            return n;
+        int a = 1, b = 2;
+        int res = 0;
+        for(int i = 3; i <= n;i++){
+            res = a + b;
+            a = b;
+            b = res;
+        }
+        
+        return res;
     }
 }
