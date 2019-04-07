c96c594764023391d083ed0bc8d08a852962a876
luocai <31661186+luocai@users.noreply.github.com>
2019-04-07 11:03:03 +0800

    Update lc101.java

--- code/lc101.java
+++ code/lc101.java
@@ -17,6 +17,34 @@ public class lc101 {
         TreeNode(int x) { val = x; }
     }
 
+    
+    
+     public boolean isSymmetric(TreeNode root) {
+        if(root == null)
+            return true;
+        return judge(root.left, root.right);
+        
+    }
+    
+    public boolean judge(TreeNode left, TreeNode right){
+        
+        if(left == null || right == null){
+            if(left != right)
+                return false;
+            else
+                return true;
+        }
+        
+        if(left.val != right.val)
+            return false;
+        
+        return judge(left.left,right.right) && judge(left.right,right.left);
+        
+    }
+    
+    
+    
+    
     public boolean isSymmetric(TreeNode root) {
         //非递归方法
         if(root==null)
