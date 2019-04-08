64caf37e0a8df64ab4fd54179cb4163daa551239
luocai <31661186+luocai@users.noreply.github.com>
2019-04-08 08:54:43 +0800

    Update lc121.java

--- code/lc121.java
+++ code/lc121.java
@@ -7,6 +7,27 @@ package code;
  * Tips：lc121, lc309, lc188, lc123, lc714
  */
 public class lc121 {
+    
+    //比较简单
+     public int maxProfit(int[] prices) {
+        
+		if(prices.length == 0)
+			return 0;
+		
+        int min = prices[0];
+        int res = 0;
+        for(int i = 0; i < prices.length; i++){
+            
+            if(prices[i] < min)
+            	min = prices[i];
+            
+            if((prices[i] - min) > res)
+            	res = prices[i] - min;
+            
+        }
+        return res;
+    }
+    
     public int maxProfit(int[] prices) {
         int min = Integer.MAX_VALUE, res=0;
         for(int i=0; i<prices.length; i++){
