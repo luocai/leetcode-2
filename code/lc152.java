c775e167d48bc87dd105e8e69ff7d894fed20ba7
luocai <31661186+luocai@users.noreply.github.com>
2019-05-04 10:55:11 +0800

    Update lc152.java

--- code/lc152.java
+++ code/lc152.java
@@ -12,19 +12,25 @@ public class lc152 {
         int[] nums = {-4,-3,-2};
         System.out.println(maxProduct(nums));
     }
-    public static int maxProduct(int[] nums) {
+    public int maxProduct(int[] nums) {
+        
+        // max  以当前元素结尾的最大连续乘积
+        // min  以当前元素结尾的最小连续乘积
         int max = nums[0], min = nums[0], res = nums[0];
-        for (int i = 1; i < nums.length ; i++) {
-            if(nums[i]>0){
-                max = Math.max(nums[i],nums[i]*max);
-                min = Math.min(nums[i],nums[i]*min);
+        
+        for(int i = 1; i< nums.length; i++){
+            
+            if(nums[i] > 0){
+                max = Math.max(nums[i], nums[i] * max);
+                min = Math.min(nums[i], nums[i] * min);
             }else{
-                int temp = max; //注意maxh会被替换，先保存下
-                max = Math.max(nums[i],nums[i]*min);
-                min = Math.min(nums[i],nums[i]*temp);
+                int temp = max; // 保存下，会变
+                max = Math.max(nums[i], nums[i] * min);  // 负负得正
+                min = Math.min(nums[i], nums[i] * temp);
             }
             res = Math.max(max, res);
         }
         return res;
+        
     }
 }
