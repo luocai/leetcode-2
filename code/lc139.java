ebd8db7225f653ed01edcf6c0e2fc214f0c051b1
luocai <31661186+luocai@users.noreply.github.com>
2019-05-04 08:11:53 +0800

    Update lc139.java

--- code/lc139.java
+++ code/lc139.java
@@ -11,15 +11,49 @@ package code;
 import java.util.List;
 
 public class lc139 {
+    //dp[i] 表示 从 0 到 i是否可以拆分
+    // dp[i] = dp[0..j] && s.substring(j,)
     public boolean wordBreak(String s, List<String> wordDict) {
+      
         boolean[] dp = new boolean[s.length()+1];
         dp[0] = true;
-        for (int i = 1; i < dp.length ; i++) {
-            for (int j = 0; j < i ; j++) {  //遍历之前计算出来的结果
-                if( dp[j]==true && wordDict.contains(s.substring(j,i)))
+        
+        for(int i = 1; i <= s.length(); i++){
+            
+            for(int j = 0; j < i; j++){
+                
+                if(dp[j] && wordDict.contains(s.substring(j,i))){
                     dp[i] = true;
+                }
+                
             }
         }
-        return dp[s.length()+1];
+        
+         return dp[s.length()]; 
+        
+    }
+    
+     //dfs  超时
+    public boolean wordBreak(String s, List<String> wordDict) {
+        
+        return dfs(s, wordDict);
+        
+    }
+    
+    public boolean dfs(String s, List<String> wordDict){
+        
+        if(s.length() == 0)
+            return true;
+        
+        boolean ans = false;
+        for(int i = 1;i  <= s.length();i++){
+            String t = s.substring(0,i);
+          
+            if(wordDict.contains(t)){
+                ans = ans || dfs(s.substring(i),wordDict);
+            }
+        }
+        return ans;
+        
     }
 }
