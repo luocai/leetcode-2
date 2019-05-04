ff88453cb2c7d32970b09e45ee2b2317352bc485
luocai <31661186+luocai@users.noreply.github.com>
2019-05-04 22:23:03 +0800

    Update lc200.java

--- code/lc200.java
+++ code/lc200.java
@@ -8,41 +8,38 @@ package code;
  * Tips：
  */
 public class lc200 {
+    
+    // dfs
     public int numIslands(char[][] grid) {
-        if(grid.length==0)
-            return 0;
-        int count = 0;
-        for (int i = 0; i < grid.length ; i++) {
-            for (int j = 0; j < grid[i].length ; j++) {
-                if( grid[i][j]=='1' ){
-                    count++;
-                    grid[i][j] = '0';   //置0， 下次不用考虑
-                    search(grid,i,j);
+        int res = 0;
+        for(int i = 0; i < grid.length;i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == '1'){
+                    dfs(grid, i, j);
+                    res++;
                 }
             }
         }
-        return count;
+        return res;
     }
-    public void search(char[][] grid, int i, int j){
-        if( i>0 && grid[i-1][j]=='1') {
-            grid[i-1][j] = '0';     //置0， 下次不用考虑
-            search(grid, i - 1, j);
-        }
-        if( j>0 && grid[i][j-1]=='1') {
-            grid[i][j-1] = '0';
-            search(grid, i, j-1);
-        }
-        if( i+1<grid.length && grid[i+1][j]=='1') {
-            grid[i+1][j] = '0';
-            search(grid, i + 1, j);
-        }
-        if( j+1<grid[0].length && grid[i][j+1]=='1') {
-            grid[i][j+1] = '0';
-            search(grid, i, j+1);
-        }
-        return;
+    
+    public void dfs(char[][] grid, int i, int j){
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length)
+            return;
+        
+        if(grid[i][j] == '0')
+            return;
+        
+        grid[i][j] = '0';
+        
+        dfs(grid, i+1,j);
+        dfs(grid, i-1,j);
+        dfs(grid, i,j+1);
+        dfs(grid, i,j-1);
+        
     }
-
+    
+    
     //并查集
     int[] flag;
     int count = 0;
