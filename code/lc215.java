58d2b141036d25c6448e02dc9115230225846058
luocai <31661186+luocai@users.noreply.github.com>
2019-05-05 09:16:27 +0800

    Update lc215.java

--- code/lc215.java
+++ code/lc215.java
@@ -12,25 +12,71 @@ public class lc215 {
         int[] nums = {3,2,3,1,2,4,5,5,6};
         System.out.println(findKthLargest(nums, 4));
     }
-    public static int findKthLargest(int[] nums, int k) {
-        return quickSort(nums, 0, nums.length-1, k);
+    public int findKthLargest(int[] nums, int k) {
+        return partion(nums, 0, nums.length-1, k);
     }
+    
+    //快排
+    public int partion(int[] nums, int s, int e, int k){
+        
+     
+        int i = s, j = e;
+        int x = nums[i];
 
-    public static int quickSort(int[] nums, int left, int right, int k){
-        int origin_l = left;        //left 和 right 移动，但要保存一下原始的值，方便递归调用
-        int origin_r = right;
-        int cur = nums[left];
-        while(left<right) {
-            while (left<right && nums[right] < cur)
-                right--;
-            nums[left] = nums[right];
-            while (left<right && nums[left] >= cur)
-                left++;
-            nums[right] = nums[left];
+        while(i < j){
+            // 注意和普通快排的区别， 此处是逆序  顺序是 nums[j] >= x
+            while(j > i && nums[j] <= x)
+                j--;
+            nums[i] = nums[j];
+            while(j > i && nums[i] >= x)
+                i++;
+            nums[j] = nums[i];
+
         }
-        nums[left] = cur;
-        if(left==k-1) return cur;
-        else if(left>k-1) return quickSort(nums, origin_l, left-1, k);
-        else return quickSort(nums, left+1, origin_r, k);
+        nums[i] = x;
+
+        if(i == k-1)
+            return x;
+        else if(i > k-1)
+            return partion(nums, s,i-1,k);
+        else 
+            return partion(nums, i+1, e, k);
+
+    }
+    
+    //堆排序
+     public int findKthLargest(int[] nums, int k) {
+        for(int i = nums.length / 2 - 1; i >= 0; i--){
+            heapAdjust(nums, i, nums.length);
+        }
+        
+        for(int i = nums.length-1; i >= nums.length -k; i--){
+            swap(nums, i, 0);
+            heapAdjust(nums, 0, i);
+        }
+        
+        return nums[nums.length - k];
+    }
+    
+    public void heapAdjust(int[] arr, int index ,int len){
+        int temp = arr[index];
+        for(int i = 2*index+1; i < len; i=2*i+1){
+            if(i+1 < len && arr[i] < arr[i+1]){
+                i++;
+            }
+            if(arr[i] > temp){
+                arr[index] = arr[i];
+                index = i;
+            }else{
+                break;
+            }
+        }
+        arr[index] = temp;
+    }
+    
+    public void swap(int[] arr ,int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
