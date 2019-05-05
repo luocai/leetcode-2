3cca01a8ca1cbf9470fa8322735846ca4a472c1c
luocai <31661186+luocai@users.noreply.github.com>
2019-05-05 11:19:00 +0800

    Update lc236.java

--- code/lc236.java
+++ code/lc236.java
@@ -17,16 +17,29 @@ public class lc236 {
         TreeNode right;
         TreeNode(int x) { val = x; }
     }
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {//递归
-        if( root==null || root==p || root==q )
+    
+    //很经典的递归
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+        if(root == null)
             return root;
+        
+        if(root == p || root == q){
+            return root;
+        }
+        
+        //分别在左右子树找
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
-        if( left!=null && right!=null )     //回溯返回。哪边不为空，返回哪边，否则返回自己。
+        
+        if(left != null && right != null) //回溯
             return root;
-        else if(left!=null)
+        else if(left != null)
             return left;
-        else return right;
+        else if(right != null)
+            return right;
+        
+        return null;
     }
 
     public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
