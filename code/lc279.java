32f0f67951e6504fca31600cc387a193f66f9ac7
luocai <31661186+luocai@users.noreply.github.com>
2019-05-05 22:39:36 +0800

    Update lc279.java

--- code/lc279.java
+++ code/lc279.java
@@ -13,6 +13,26 @@ public class lc279 {
     public static void main(String[] args) {
         System.out.println(numSquares(12));
     }
+    
+    // dp[i] 表示 i 的最少平方数
+    // dp[i] = min(dp[i], dp[i-j*j] + 1)
+    public int numSquares(int n) {
+        if(n == 0)
+            return 0;
+        
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        
+        for(int i = 2 ;i <= n;i++){
+            dp[i] = Integer.MAX_VALUE;
+            for(int j = 1; j*j <= i; j++){
+                
+                dp[i] = Math.min(dp[i], dp[i-j*j]+1);
+            }
+        }
+        return dp[n];
+    }
+    
     public static int numSquares(int n) {
         int[] dp = new int[n];
         Arrays.fill(dp,Integer.MAX_VALUE);
