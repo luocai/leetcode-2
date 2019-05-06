b7064d9705ea93bb7eeae03147e16b1225fda922
luocai <31661186+luocai@users.noreply.github.com>
2019-05-06 09:17:16 +0800

    Update lc287.java

--- code/lc287.java
+++ code/lc287.java
@@ -11,6 +11,59 @@ package code;
  *       lc268 lc448 lc287
  */
 public class lc287 {
+    
+     //使用数组配合下标，抽象成链表问题
+    //nums = [2,5, 9 ,6,9,3,8, 9 ,7,1]，构造成链表就是：2->[9]->1->5->3->6->8->7->[9]，也就是在[9]处循环。
+    public static int findDuplicate(int[] nums) {
+	      
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        
+        // 因为有环，必然相遇，会在环内的任何一个节点相遇
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        
+        // 寻找环入口
+        fast = 0;  
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+		  
+	 }
+    
+    // 归位大法，是错误的，与题意不符合
+    public static int findDuplicate(int[] nums) {
+	        
+		  //每次遍历归位一个元素  1 2 3
+	        for(int i = 0; i < nums.length ;){
+	        	
+	        	//记住，只有昂该位归位， i才要+
+	            if(nums[i] == i+1){
+	            	i++;
+	            	continue;
+	            }
+	            
+	            // 需要归位的位置
+	            int index = nums[i]-1;
+	            
+	            //判断该位置是否已经归位，因为nums[i] 要换过去， 如果目标位置有了，就重复
+	            if(nums[index] == nums[i]){
+	                return nums[index];
+	            }
+	            //如果未归位， 则交换这两个位置
+	            int temp = nums[index];
+	            nums[index] = nums[i];
+	            nums[i] = temp;
+	            
+	        }
+	        return -1;
+	 }
+    
+    
     public int findDuplicate(int[] nums) {  //该方法修改了数组，是错误的，没看清题意
         for (int i = 0; i < nums.length ; i++) {
             if(nums[i]!=i+1){
