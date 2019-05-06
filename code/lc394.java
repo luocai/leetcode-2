ad819c7c0a3cecf50ce21e7eaf8bf196cafd4b27
luocai <31661186+luocai@users.noreply.github.com>
2019-05-06 23:04:59 +0800

    Update lc394.java

--- code/lc394.java
+++ code/lc394.java
@@ -10,6 +10,55 @@ package code;
 import java.util.Stack;
 
 public class lc394 {
+    
+    // 栈
+     public String decodeString(String s) {
+        
+        LinkedList<String> stack = new LinkedList();
+        
+        // 特别注意字符串操作的顺序   count = stack.pop() + count 和 count = count + stack.pop()
+        for(int i = 0; i < s.length() ; i++){
+            
+            if(s.charAt(i) == ']'){
+                
+                String str = "";
+                //弹弹弹，弹到底
+                while(!stack.peek().equals("[")){
+                    str = stack.pop() + str;
+                }
+                
+                stack.pop(); // 弹出 [
+                
+                //计算数字
+                String count = "";
+                while(!stack.isEmpty() && stack.peek().charAt(0) >= '0' && stack.peek().charAt(0) <= '9'){
+                    count = stack.pop() + count;
+                }
+                
+                int c = Integer.parseInt(count);
+                // 循环，然后放入栈中
+                String tar = "";
+                for(int j = 0; j < c;j++){
+                    tar += str;
+                }
+                stack.push(tar);
+                
+            }else{
+                stack.push(""+s.charAt(i));
+            }
+            
+        }
+        
+        String res = "";
+        while(!stack.isEmpty()){
+            res = stack.pop() + res;
+        }
+        return res;
+        
+    }
+    
+    
+    
     public static void main(String[] args) {
         System.out.println(decodeString("2[abc]3[cd]ef"));
     }
