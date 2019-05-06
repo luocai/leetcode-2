233e357af1a00db0b529c620ad2ad845f44ba639
luocai <31661186+luocai@users.noreply.github.com>
2019-05-06 10:32:10 +0800

    Update lc312.java

--- code/lc312.java
+++ code/lc312.java
@@ -14,25 +14,31 @@ public class lc312 {
     public static void main(String[] args) {
         System.out.println(maxCoins2(new int[]{3,1,5,8}));
     }
-    public static int maxCoins(int[] iNums) {
-        int[] nums = new int[iNums.length + 2];
-        int n = 1;
-        for (int x : iNums) if (x > 0) nums[n++] = x;
-        nums[0] = nums[n++] = 1;
-        int[][] memo = new int[n][n];
-        int res =  burst(memo, nums, 0, n - 1);
-        return res;
-    }
-
-    public static int burst(int[][] memo, int[] nums, int left, int right) {
-        if (left + 1 == right) return 0;
-        if (memo[left][right] > 0) return memo[left][right];
-        int ans = 0;
-        for (int i = left + 1; i < right; ++i)
-            ans = Math.max(ans, nums[left] * nums[i] * nums[right]
-                    + burst(memo, nums, left, i) + burst(memo, nums, i, right));
-        memo[left][right] = ans;
-        return ans;
+    //区间dp
+    //dp[left][right] 表示 （left,right）区间内的得分
+    //dp[left][right] = newnums[left]*newnums[i]*newnums[right] + dp[left][i] +dp[i][right]
+    public int maxCoins(int[] nums) {
+        
+        int n = nums.length + 2;
+        int[][] dp = new int[n][n];
+        int[] newnums = new int[n];
+        // 构造新数组，-1 和 n加上去
+        for(int i = 0; i < nums.length; i++){
+            newnums[i+1] = nums[i];
+        }
+        newnums[0] = newnums[n-1] = 1;
+        
+        for(int k = 2 ;  k < n; k++){
+            for(int left = 0; left < n - k ;left++){
+                int right = left + k; //右边边界
+                for(int i = left + 1;  i< right ;i++){
+                    dp[left][right] = Math.max(dp[left][right], newnums[left]*newnums[i]*newnums[right]+ dp[left][i]+dp[i][right]);
+                }
+            }
+        }
+        
+        // n-1
+        return dp[0][n-1];
     }
 
     public static int maxCoins2(int[] iNums) {
