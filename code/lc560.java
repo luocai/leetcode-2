90ab0d2f26660384de62dcf5a76f1e297d0fa11c
luocai <31661186+luocai@users.noreply.github.com>
2019-05-09 08:52:08 +0800

    Update lc560.java

--- code/lc560.java
+++ code/lc560.java
@@ -12,6 +12,29 @@ import java.util.HashMap;
  *       lc303, lc437, lc560
  */
 public class lc560 {
+    
+    //求出累加和存在hashmap中，如果当前hashmap中存在sum-k，那么就是一个解
+    public int subarraySum(int[] nums, int k) {
+       
+        Map<Integer, Integer> map = new HashMap();
+        // 如果sum == k ， map.get(sum-k) == 1， 是一个解
+        map.put(0,1);
+        
+        int sum = 0, res  = 0;
+        for(int i = 0; i < nums.length ;i++){
+            sum += nums[i];
+            
+            if(map.containsKey(sum - k))
+                res += map.get(sum-k);   // sum1+sum2 = k ，说明sum2-sum1那一段是一个解
+            map.put(sum, map.getOrDefault(sum,0)+1);
+            System.out.println(sum + "  " + map.get(sum));
+        }
+        return res;
+    }
+    
+    
+    
+    
     public int subarraySum(int[] nums, int k) {
         HashMap<Integer, Integer> hs = new HashMap();
         hs.put(0,1);
