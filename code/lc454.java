d867a2c63c120d38c2760d1e00e65e23dc5641bb
luocai <31661186+luocai@users.noreply.github.com>
2019-05-16 09:16:22 +0800

    Update lc454.java

--- code/lc454.java
+++ code/lc454.java
@@ -13,6 +13,42 @@ import java.util.HashMap;
  * Tips：
  */
 public class lc454 {
+    
+    //两个哈希
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+		
+		Map<Integer, Integer> map = new HashMap<>();
+		
+		int n = A.length;
+		
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				int temp = A[i] + B[j];
+				if(map.containsKey(temp)){
+					map.put(temp, (int)map.get(temp)+1);
+				}else{
+					map.put(temp, 1);
+				}
+			}
+		}
+		
+		int res = 0;
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				
+				int temp = C[i] + D[j];
+				if(map.containsKey((-1)*temp)){
+					res += map.get((-1)*temp);
+				}
+				
+			}
+		}
+		
+		
+		return res;
+    }
+    
+    
     public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
         HashMap<Integer, Integer> hm = new HashMap();
         int res=0;
