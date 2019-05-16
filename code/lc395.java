b9a3e9db3bea981b6ca6eaee3512d32f1b111946
luocai <31661186+luocai@users.noreply.github.com>
2019-05-16 09:38:48 +0800

    Update lc395.java

--- code/lc395.java
+++ code/lc395.java
@@ -10,6 +10,35 @@ package code;
  *       想到了利用不足K的分割点作为切点，挺难的
  */
 public class lc395 {
+    
+    //核心思想：如果某个字符 x 在整个字符串中出现的次数 <k，那么 x 不可能出现在最终要求的子串中。因此，可以将原字符串截断为：
+    //x 左侧字符子串 + x + x 右侧字符子串
+    public int longestSubstring(String s, int k) {
+        
+        return solution(s, 0, s.length()-1, k);
+    }
+    
+    public int solution(String s, int left, int right, int k){
+        
+        int[] ch = new int[26];
+        for(int i = left; i <= right; i++){
+            ch[s.charAt(i) - 'a']++;
+        }
+        
+        for(int i = left; i <= right; i++){
+            
+            if(ch[s.charAt(i) - 'a'] < k){
+                int max_left = solution(s,left, i-1, k);
+                int max_right = solution(s, i+1, right, k);
+                return Math.max(max_left, max_right);
+            }
+        }
+        return right - left +1;
+    }
+    
+    
+    
+    
     public int longestSubstring(String s, int k) {
         int res = 0;
         for (int i = 1; i <= 26 ; i++) {
