0b484b392072354adbff0248315068d1884c686b
luocai <31661186+luocai@users.noreply.github.com>
2019-05-17 10:34:56 +0800

    Update lc329.java

--- code/lc329.java
+++ code/lc329.java
@@ -8,6 +8,50 @@ package code;
  * Tips：
  */
 public class lc329 {
+    
+    public int longestIncreasingPath(int[][] matrix) {
+        
+        if(matrix.length == 0 || matrix[0].length ==0)
+            return 0;
+        
+        //用来记忆
+        int[][] cache = new int[matrix.length][matrix[0].length];
+        int res = 0;
+        
+        for(int i = 0; i < matrix.length ;i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                res = Math.max(dfs(matrix, i, j, cache),res);
+            }
+        }
+        
+        return res;
+    }
+    
+    public int dfs(int[][] matrix, int i ,int j, int[][] cache){
+        
+        // 如果已经访问过了，则合并
+        if(cache[i][j] != 0)
+            return cache[i][j];
+        int max = 1;
+        
+        if(i-1 >= 0 && matrix[i-1][j] > matrix[i][j])
+            max = Math.max(dfs(matrix,i-1,j,cache)+1, max);
+        
+        if(i+1 < cache.length && matrix[i+1][j] > matrix[i][j])
+            max = Math.max(dfs(matrix,i+1,j,cache)+1, max);
+        
+        if(j-1 >= 0 && matrix[i][j-1] > matrix[i][j])
+            max = Math.max(dfs(matrix,i,j-1,cache)+1, max);
+        
+        if(j+1 < cache[0].length && matrix[i][j+1] > matrix[i][j])
+            max = Math.max(dfs(matrix,i,j+1,cache)+1, max);
+        
+        cache[i][j] = max;
+        return max;
+    }
+    
+    
+    ......................................
     public int longestIncreasingPath(int[][] matrix) {
         if(matrix.length==0) return 0;
         int[][] cache = new int[matrix.length][matrix[0].length];   //存储计算过的结果
