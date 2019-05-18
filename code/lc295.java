d1340ddf00df7e3929f85e04b0a50abcdcb80514
luocai <31661186+luocai@users.noreply.github.com>
2019-05-18 10:44:17 +0800

    Update lc295.java

--- code/lc295.java
+++ code/lc295.java
@@ -11,6 +11,51 @@ import java.util.PriorityQueue;
  * Tips：
  */
 public class lc295 {
+    
+      // 用两个优先队列
+    PriorityQueue<Integer> minHeap;
+    PriorityQueue<Integer> maxHeap;
+    int flag = 0; // 标志位，判断加入哪个堆
+    /** initialize your data structure here. */
+    public MedianFinder() {
+        minHeap = new PriorityQueue();
+        maxHeap = new PriorityQueue(new Comparator<Integer>(){
+            
+            public int compare(Integer a, Integer b){
+                return b - a;
+            }
+        });
+    }
+    
+    public void addNum(int num) {
+        
+        if(flag == 0){
+            // 先放入小根堆，然后取出堆顶（最小的） 放入大根堆
+            minHeap.offer(num);
+            maxHeap.offer(minHeap.poll());
+            flag = 1;
+            
+        }else{
+            //先放入大根堆，然后取出堆顶（最大的）  放入小根堆
+            maxHeap.offer(num);
+            minHeap.offer(maxHeap.poll());
+            flag = 0;
+        }
+        //这种机制保证了 小根堆都是大的那一半，大根堆都是小的那一半
+        
+    }
+    
+    public double findMedian() {
+        
+        if(flag == 1){
+            return maxHeap.peek();
+        }else{
+            return (maxHeap.peek()+minHeap.peek()) / 2.0;
+        }
+    }
+    
+    
+    ----------------------------------------------------
     class MedianFinder {
         PriorityQueue<Integer> pq1;    //默认是最小，右半边
         PriorityQueue<Integer> pq2;    //左半边
