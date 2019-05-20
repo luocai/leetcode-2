d32f8842b2d0b909af2d74474dc884f4ad53f34f
luocai <31661186+luocai@users.noreply.github.com>
2019-05-20 10:34:18 +0800

    Update lc230.java

--- code/lc230.java
+++ code/lc230.java
@@ -11,6 +11,26 @@ import java.util.Stack;
  * Tips：非递归中序遍历还是不熟练。。。
  */
 public class lc230 {
+    
+    int count = 0;
+    int res = 0;
+    public int kthSmallest(TreeNode root, int k) {
+        inOrder(root, k);
+        return res;
+    }
+    
+    public void inOrder(TreeNode root, int k){
+        if(root == null)
+            return;
+        
+        inOrder(root.left, k);
+        count++;
+        if(count == k)
+            res = root.val;
+        inOrder(root.right ,k);
+    }
+    
+    ----------------
     public class TreeNode {
         int val;
         TreeNode left;
