bb4e6a3c0d607f6b96f1483e3c71b49a881ee034
luocai <31661186+luocai@users.noreply.github.com>
2019-05-20 09:40:44 +0800

    Update lc238.java

--- code/lc238.java
+++ code/lc238.java
@@ -8,6 +8,31 @@ package code;
  * Tips：
  */
 public class lc238 {
+    
+     // 从左至右乘一遍，从右到左乘一遍
+    public int[] productExceptSelf(int[] nums) {
+        
+        int[] res = new int[nums.length];
+        
+        int help = 1;
+        
+        //从左到右乘一遍，这样每个位置的值就是这个数左边所有数的乘积（不包括该数）
+        for(int i = 0; i < nums.length ;i++){
+            res[i] = help;
+            help *= nums[i];
+        }
+        
+        help = 1;
+        //从右到左乘一遍，这样每个位置的值就是这个数右边所有数的乘积（不包括该数）
+        for(int i = nums.length-1; i >= 0 ;i--){
+            res[i] *= help;
+            help *= nums[i];
+        }
+        return res;
+    }
+    
+    
+    -----------------------------
     public int[] productExceptSelf(int[] nums) {
         int[] res = new int[nums.length];
         res[0] = 1;
