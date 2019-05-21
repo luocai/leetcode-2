86bd19af78e59364afc678d09f5346144946cec3
luocai <31661186+luocai@users.noreply.github.com>
2019-05-21 09:36:06 +0800

    Update lc202.java

--- code/lc202.java
+++ code/lc202.java
@@ -11,6 +11,36 @@ package code;
 import java.util.HashSet;
 
 public class lc202 {
+    
+     // 如果不是快乐数， 则肯定会重复出现，所以用一个set进行记录即可
+    public boolean isHappy(int n) {
+        
+        Set<Integer> set = new HashSet();
+        
+        while(true){
+            if(n == 1)
+                return true;
+            
+            if(set.contains(n))
+                return false;
+            set.add(n);
+            n = cal(n);
+        }
+        
+    }
+    
+    
+    public int cal(int n){
+        int res = 0;
+        while(n > 0){
+            int t = n % 10;
+            res += t*t;
+            n = n / 10;
+        }
+        return res;
+    }
+    
+    ------------------------------------
     public boolean isHappy(int n) {
         HashSet hs = new HashSet<Integer>();
         hs.add(n);
