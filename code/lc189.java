d55b2e29a97c66e3937c1621cdc4eb2b8f327ea8
luocai <31661186+luocai@users.noreply.github.com>
2019-05-21 14:01:09 +0800

    Update lc189.java

--- code/lc189.java
+++ code/lc189.java
@@ -9,6 +9,29 @@ package code;
  * Tips：
  */
 public class lc189 {
+    
+    // 先 0 - n-1 翻转
+    // 再 0 - k-1 翻转
+    // 再 k - n-1翻转
+    public void rotate(int[] nums, int k) {
+        
+        k = k % nums.length;
+        reverse(nums, 0, nums.length-1);
+        reverse(nums, 0, k -1);
+        reverse(nums, k, nums.length-1);
+        
+    }
+    
+    public void reverse(int[] nums, int s, int e){
+        while(s < e){
+            int temp = nums[s];
+            nums[s++] = nums[e];
+            nums[e--] = temp;
+        }
+    }
+    
+    
+    ----------------------
     public void rotate(int[] nums, int k) {
         if(nums.length<2) return;
         int sum = 0;
