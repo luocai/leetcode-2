f1473f78c4d5c83748f8ebc0ee5d96c329718eb7
luocai <31661186+luocai@users.noreply.github.com>
2019-05-23 10:58:58 +0800

    Update lc130.java

--- code/lc130.java
+++ code/lc130.java
@@ -8,6 +8,49 @@ package code;
  * Tips：
  */
 public class lc130 {
+    
+    
+     // 对边界的点做dfs，把 O换成 Y
+    // 遍历整个图， 把 O 换成 X, Y 换成 O
+    public void solve(char[][] board) {
+        if(board.length == 0 || board[0].length == 0)
+            return ;
+        int row = board.length;
+        int col = board[0].length;
+        
+        for(int i = 0; i < row; i++){
+            dfs(board, i, 0);
+            dfs(board, i, col-1);
+        }
+        for(int i = 0; i < col ; i++){
+            dfs(board, 0, i);
+            dfs(board, row -1, i);
+        }
+        
+        for(int i = 0; i < row ;i++){
+            for(int j = 0; j < col ;j++){
+                if(board[i][j] == 'O')
+                    board[i][j] = 'X';
+                else if(board[i][j] == 'Y')
+                    board[i][j] = 'O';
+            }
+        }
+    }
+    
+    public void dfs(char[][] board, int i, int j){
+        
+        if( i < 0 || i >= board.length || j < 0 || j >= board[0].length || 
+          board[i][j] == 'X' || board[i][j] == 'Y')
+            return ;
+        
+        board[i][j] = 'Y';
+        dfs(board, i+1, j);
+        dfs(board, i-1, j);
+        dfs(board, i, j+1);
+        dfs(board, i, j-1);
+    }
+    
+    --------------------
     public static void main(String[] args) {
         char[][] board = {{'X','X','X','X'},{'X','O','O','X'},{'X','X','O','X'},{'X','O','X','X'}};
         solve(board);
