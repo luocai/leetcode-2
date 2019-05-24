96d49fbefda600406be3fa9a9deb8b8af7eea826
luocai <31661186+luocai@users.noreply.github.com>
2019-05-24 13:39:04 +0800

    Update lc108.java

--- code/lc108.java
+++ code/lc108.java
@@ -8,6 +8,27 @@ package code;
  * Tips：Bingo!
  */
 public class lc108 {
+    
+    // 二分递归
+     public TreeNode sortedArrayToBST(int[] nums) {
+        return solution(nums, 0, nums.length-1);
+    }
+    
+    public TreeNode solution(int[] nums, int l, int r){
+        if(l > r)
+            return null;
+        
+        int m = l + (r - l) / 2;
+  
+        TreeNode root = new TreeNode(nums[m]);
+        root.left = solution(nums, l, m-1);
+        root.right = solution(nums, m+1, r);
+        return root;
+    }
+    
+    
+    
+    --------------------------
     public class TreeNode {
         int val;
         TreeNode left;
