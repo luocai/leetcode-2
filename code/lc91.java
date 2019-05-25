40ae54153ca77f2bf9520258bb423e3d5903b7d4
luocai <31661186+luocai@users.noreply.github.com>
2019-05-25 08:59:30 +0800

    Update lc91.java

--- code/lc91.java
+++ code/lc91.java
@@ -8,6 +8,47 @@ package code;
  * Tips：Bingo!
  */
 public class lc91 {
+    // 超级经典的
+     public int numDecodings(String s) {
+        
+        if(s.length() == 0)
+            return 0;
+        
+        int[] dp = new int[s.length()+1];
+        dp[0] = 1;
+        
+        if(s.charAt(0) == '0')
+            return 0;
+        else
+            dp[1] = 1;
+        
+        // 对s的每个字符， 除了判断它的前两位外，还会判断当前位
+        for(int i = 2; i <= s.length() ;i++){
+            
+            int a = s.charAt(i-2) - '0';
+            int b = s.charAt(i-1) - '0';
+            
+            
+            if((a*10+b) <= 26 && (a*10+b) >= 10)
+                dp[i] += dp[i-2];
+            
+            // 如果是 10 20 这种， 那么这里就不会执行
+            // i+1位的时候， 上面那个判断就不会执行，如果此时 i+1 为0，就返回0
+            if(b > 0 && b <= 9)
+                dp[i] += dp[i-1];
+            
+            //解析错误，无法解码
+            if(dp[i] == 0)
+                return 0;
+        }
+        
+        return dp[s.length()];
+    }
+    
+    
+    
+    
+    --------------------------------
     public static void main(String[] args) {
         System.out.println(numDecodings("99"));
     }
