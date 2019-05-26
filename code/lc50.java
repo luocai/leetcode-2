c25545f0f9ff886808d47ca4a5da9c632aa46cc7
luocai <31661186+luocai@users.noreply.github.com>
2019-05-26 10:56:40 +0800

    Update lc50.java

--- code/lc50.java
+++ code/lc50.java
@@ -8,6 +8,32 @@ package code;
  * Tips：注意溢出的情况，负数的情况。
  */
 public class lc50 {
+    // 类似二分
+    public double myPow(double x, int n) {
+        
+        if(n == 0)
+            return 1;
+        
+        // 特殊情况
+        if(n==Integer.MIN_VALUE && x!=1 && x!=-1)
+            return 0;
+        
+        // 如果n是负数，则变成小数
+        if(n < 0){
+            n = -n;
+            x = 1/ x; 
+        }
+        
+        // 如果幂是 2的倍数， 则x变成平方， n除以2
+        // 如果幂不是2的倍数， 则给它乘x，让他变成2的倍数
+        if( n % 2 == 0)
+            return myPow(x*x, n / 2);
+        else
+            return x*myPow(x*x, n / 2);
+    }
+    
+    ---------------------
+        
     public double myPow(double x, int n) {
         if(n == 0) return 1;
         if(n==Integer.MIN_VALUE && x!=1 && x!=-1) return 0;
