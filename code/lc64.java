0ee53e0657e1bbba088a02663e93e80e8c908f58
luocai <31661186+luocai@users.noreply.github.com>
2019-07-05 07:18:38 +0800

    Update lc64.java

--- code/lc64.java
+++ code/lc64.java
@@ -12,6 +12,32 @@ public class lc64 {
         System.out.println(minPathSum(grid));
     }
     
+     public int minPathSum(int[][] grid) {
+        
+        if(grid.length == 0 || grid[0].length == 0)
+            return 0;
+        
+        int[][] dp = new int[grid.length][grid[0].length];
+        dp[0][0] = grid[0][0];
+        
+        for(int i = 1; i < dp.length; i++){
+            dp[i][0] = grid[i][0] + dp[i-1][0];
+        }
+        
+        for(int i = 1; i < dp[0].length; i++){
+            dp[0][i] = grid[0][i] + dp[0][i-1];
+        }
+        
+        for(int i = 1; i< dp.length; i++){
+            for(int j = 1; j < dp[0].length; j++){
+                
+                dp[i][j] = grid[i][j] + Math.min(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        
+        return dp[dp.length-1][dp[0].length-1];
+    }
+
     //dp  dp[i][j] 表示 （0,0）到（i,j）的最小值
     // dp[i][j] = min(dp[i-1][j],dp[i][j-1])+ grid[i][j]
     public static int minPathSum(int[][] grid) {
